9efb6141469d1509362b01fca9969a18d92c305d
SoyeonSyncoms <SoyeonKim@syncoms.co.uk>
2015-09-29 17:57:16 +0100

    add slide for promotions

--- js/data_UK.js
+++ js/data_UK.js
@@ -134,6 +134,132 @@ var data = {
       }
     ] 
 };
+var Mobildata = { 
+  promotion_text_align:"right",
+
+  // --- top banners that vertical slider shows...
+  promotion_mobile: [
+       { 
+        id:"promo1",
+        url: "http://www.printerpix.co.uk/photo-books/PhotoHardCover/", // link to go
+        img_url: "./img/promotions/blue.png", 
+        title:"",
+        description:"Print Your Memories<br>on Photo Book", //promotion description
+        text_color:"#414141",
+        promo_des:"Elegance and Simplicity<br> to make your memories unique",
+        button: "Personalise Now" , 
+        button_color:"#cf207e",       
+        customer_review:"" },     
+       { 
+        id:"promo2",
+        url: "http://www.printerpix.co.uk/photo-mugs/", 
+        img_url: "./img/promotions/green.png", 
+        title:"",
+        description:"<strong>Have a great start<br> of the day</strong>",
+        text_color:"#414141",
+        promo_des:"With a Personalised Photo Mug",
+        button: "Personalise Now" ,     
+        button_color:"#cf207e", 
+        customer_review:"" },     
+      { 
+        id:"promo3",
+        url: "http://www.printerpix.co.uk/photo-canvas/", 
+        img_url: "./img/promotions/red.png", 
+        title:"",
+        description:"<strong>Never forget<br>your special days</strong>",
+        text_color:"#414141",
+        promo_des:"with our personalised <br>Luxury Canvas",
+        button: "Personalise Now" ,  
+        button_color:"#cf207e",        
+        customer_review:"" },     
+       { 
+        id:"promo4",
+        url: "http://www.printerpix.co.uk/iphone-cases/", 
+        img_url: "./img/promotions/yellow.png", 
+        title:"",
+        description:"<strong>Your Happy Moments</strong>",
+        text_color: "#414141",
+        promo_des: "On a Special, Personalised,<br> Photo Phone Cover",
+        button: "Personalise Now",
+        button_color:"#cf207e",   
+        customer_review:"" },
+ 
+        ],
+  bestSeller:[
+       
+      {   
+        url:  "/photo-books/ValentinoWhite", 
+        img_url: "./img/Themes/bestSeller/UK/Photo-Book-Valentina.jpg",
+        customerStar: "./img/Themes/bestSeller/stars/star_4_5.png",
+        title: "Valentina Photo book", 
+        description: "Perfect for weddings, birthdays and other celebrations, this Valentina Photobook is the perfect way to"},
+     { 
+        url: "/montage-canvas", 
+        img_url: "./img/Themes/bestSeller/UK/Montage-Canvas.jpg",
+        customerStar: "./img/Themes/bestSeller/stars/star_4_5.png",
+        title: "Collage Canvas",  
+        description: "Record your memories in style with this fully customisable canvas. " },
+ 
+     { 
+       url: "/instagram-canvas/", 
+        img_url: "./img/Themes/bestSeller/UK/Instagram_Canvas.jpg",
+        customerStar:"./img/Themes/bestSeller/stars/star_4_5.png",
+        title: "Instagram Canvas",  
+        description: "Perfect for publishing your favourite Instagram photos, this sturdy square canvas is a great way to decorate your home or office." },
+        
+       { 
+        url: "/metal-prints/", 
+        img_url: "./img/Themes/bestSeller/UK/Metal-Print.jpg", 
+        customerStar:"./img/Themes/bestSeller/stars/no_star.png",
+        title: "Metal Prints", 
+        description: "Upload a photo or select an image from our huge online library to print on this sturdy aluminium sheet" },
+
+        ],
+  instagram: [
+      { 
+        url: "/photo-magic-mug/",
+        img_url: "./img/Themes/bestSeller/UK/Magic-Mug.jpg", 
+        customerStar:"./img/Themes/bestSeller/stars/star_4_5.png",
+        isNew:"",
+        title: "Magic Mug",  
+        description:"Using the latest heat-to-reveal technology, simply pour a hot drink to see your favourite photo magically appear." },
+      { 
+        url: "/Jigsaws-Wooden/", 
+        img_url: "./img/Themes/bestSeller/UK/Wood-Jigsaw.jpg",
+        customerStar: "./img/Themes/bestSeller/stars/star_4_5.png",
+        isNew:"",
+        title: "Wooden Jigsaw",         
+        description: "Great fun for the whole family, upload a photo or choose an image from our huge library to print on this full-colour jigsaw." },
+      { 
+        url: "/photo-cushion-gifts/", 
+        img_url: "./img/Themes/bestSeller/UK/Cushion-Cover.jpg",
+        customerStar:"./img/Themes/bestSeller/stars/star_4_5.png",
+        isNew:"",
+        title: "Cushion Cover", 
+        description: "Decorate your home with this personalised Cushion Cover." },
+      {   
+        url: "/Crystal-Photo-Frame", 
+        img_url: "./img/Themes/bestSeller/UK/Crystal-Frame.jpg",
+        customerStar:"./img/Themes/bestSeller/stars/no_star.png",
+        isNew:"",
+        title: "Crystal Frame", 
+        description: "Sleek and sophisticated, print your favourite photos on this high quality crystal block." },
+               
+  ],
+   advertise: [
+      { 
+          app_url: "www.printerpix.co.uk",  //TODO: need to modify
+          app_img_url: "./img/Themes/banner/App-Banner.jpg", 
+          app_title:"MobileApplication", 
+          app_description: "Redirect to mobile application download page" ,
+
+          getFree_url: "http://www.printerpix.co.uk/e40fp/",  
+          getFree_img_url:"./img/Themes/banner/40-Prints-Banner.jpg", 
+          getFree_titile: "Free Photos",
+          getFree_description: "To get 40 Free Photos"
+      }
+    ] 
+};
 
 var menu = { 
   login: [
